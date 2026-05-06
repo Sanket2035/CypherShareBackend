a9932db484fda9873009c091db77e98e83a26456
Shinde Sanket <sanketks200305@gmail.com>
2026-05-06 21:17:30 +0530

    Configure WebSocket container with buffer sizes
    Added a bean to configure WebSocket message buffer sizes.

--- src/main/java/com/cyphershare/config/CypherShareWebSocketConfig.java
+++ src/main/java/com/cyphershare/config/CypherShareWebSocketConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
 @Configuration
 @EnableWebSocket
 @SuppressWarnings("all")
@@ -21,5 +24,14 @@ public class CypherShareWebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chunkWebSocketHandler, "/ws/relay")
                 .setAllowedOriginPatterns("*");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        // Set max binary message size to 10MB (frontend sends chunks of 5MB)
+        container.setMaxBinaryMessageBufferSize(10 * 1024 * 1024);
+        container.setMaxTextMessageBufferSize(10 * 1024 * 1024);
+        return container;
     }
 }
